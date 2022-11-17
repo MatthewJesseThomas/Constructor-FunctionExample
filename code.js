4a544756123ebc39169ad5674e3da55b93bff4ed
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-17 15:28:18 +0200

    main

--- code.js
+++ code.js
@@ -30,15 +30,23 @@ Make use of the prototype to add a new property.
 
 // •A factory function can be defined as a function that creates an object and returns it. It is similar to constructor functions/class functions. The factory function is a very useful tool in JavaScript since it returns the object of any class directly.
 // #Eg.
-let john = {
-    firstName: 'Zenku',
-    lastName: 'Dono',
-    getFullName() {
-        return this.firstName + ' ' + this.lastName;
+function creator(firstName, lastName, _class,  type) {
+    return {
+      firstName: firstName,
+      lastName: lastName,
+      class: _class,
+      type: type,
+      getFullName() {
+          return firstName + ' ' + lastName;
+      }
     }
-};
+}
+let classes = ["A-ThreatLevel", "B-ThreatLevel", "C-ThreatLevel", "D-ThreatLevel", "E-ThreatLevel", "F-ThreatLevel", "S-ThreatLevel"]
+let type = ["AGodLevel", "BDragonLevel", "CDemonLevel", "DTigerLevel", "EWolfLevel", "FNormalLevel", "SCreationLevel"]
+let precursor = creator('Zenku', 'Dono', classes, type)
 
-console.log(john.getFullName());
+console.table(precursor);
+console.log(precursor.getFullName());
 
 // What is a Constructor-Function~
 
@@ -47,27 +55,53 @@ console.log(john.getFullName());
 /* A constructor in Javascript is used to create a new object and set values
 for any existing object properties*/
 
-class Animal{
-  
-  constructor (name, height, weight){
-    console.log("created the Kaiju named", name)
+
+
+function Animal(name, height, weight) {
+  this.name = name,
+  this.height = height,
+  this.weight = weight
+}
+Animal.prototype.nameLength = () => {
+  return this.name.length
+}
+
+
+class Kaiju {
+  constructor(name, height, weight) {
+    this.name = name,
+    this.height = height,
+    this.weight = weight;
   }
   nameLength() {
-    return name.length;
-    
+    return this.name.length;
   }
-  
 }
- var dog = new Animal("Daimajin", 25, 90);
- var fish = new Animal("Gamera", 2, .02);
 
-	console.log(dog.nameLength);
+console.table("created the Kaiju named", name)
+
+
+let kingKaiju1 = new Kaiju(
+  prompt('Enter Desired Kaiju:'),
+  prompt('Enter their Height:'),
+  prompt('Enter their Weight:'),
+  prompt('Enter their Class:'),
+  prompt('Enter their Threat-Level:')
+)
+
+let kingGamera = new Kaiju("Gamera", 78+'Metres', 10000+'Tons');
+
+console.table(kingKaiju1);
+console.table(kingGamera);
 // What is the Difference Between Factory-Function and Constructor-Functions~
-class Fuck {
+class InterDimensional {
+  constructor() {
+    this.dimension = 'Overworld'
+  }
   who() {
-    return 'Fuck-You'
+    return 'Sunpō no rokku'
   }
 }
-let fuck = new Fuck()
-console.log(fuck.who());
+let dimensionalLock = new InterDimensional()
+console.table(dimensionalLock);
 // •If a Factory-Function can be defined as a function that creates an object and returns it. It is similar to constructor functions/class functions. The factory function is a very useful tool in JavaScript since it returns the object of any class directly, then the Constructor-Function is a function that creates an instance of a class which is typically called an “object”. In JavaScript, a constructor gets called when you declare an object using the new keyword. The purpose of a constructor is to create an object and set values if there are any object properties present.
